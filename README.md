5a038a4177fb37582860860611abc4aeeb909e9e
Michel Melo <michellander@gmail.com>
2020-04-02 11:33:19 +0100

    init track

--- README.md
+++ README.md
@@ -1,2 +1,64 @@
-# dpd-track-trace
-Receber uma encomenda Track &amp; Trace
+# michelmelo/dpd-track-trace
+#### DPD Tracking (Portugal post office) tracking for PHP.
+
+track mail and packages using CTT service (Portuguese post office)
+https://dpd.pt/track-and-trace
+
+## Requirements
+
+PHP 5.6 or higher.
+
+## Installation
+
+Include michelmelo/dpd-track-trace in your project, by adding it to your composer.json file.
+```php
+{
+    "require": {
+        "michelmelo/dpd-track-trace": "1.*"
+    }
+}
+```
+
+
+## Usage
+
+```php
+$dpd = new \MichelMelo\Dpd\DpdTracking();
+$res = $dpd->trackObjects(['ED123456789PT', 'LX123456789PT']);
+var_dump($res);
+```
+
+```php
+/*
+output:
+Array
+(
+    [ED123456789PT] => Array
+        (
+            [status] => 6
+            [statusText] => Objeto não encontrado
+        )
+
+    [LX123456789PT] => Array
+        (
+            [status] => 4
+            [statusText] => Objeto entregue
+        )
+
+)
+*/
+```
+
+## ChangeLog
+
+ - initial release
+
+## Contributing
+
+ - welcome to discuss a bugs, features and ideas.
+
+## License
+
+michelmelo/dpd-track-trace is release under the MIT license.
+
+You are free to use, modify and distribute this software
---
+++ src/DpdTracking.php
@@ -0,0 +1,103 @@
+<?php
+
+namespace MichelMelo\Dpd;
+
+use Exception;
+use Goutte\Client;
+
+class DpdTracking
+{
+    public $refsPerRequest   = 25; // maximum number of references per request. max allowed by Dpd is 25
+    public $waitAfterRequest = 0; // seconds
+
+    const STATUS_PENDING          = 1;
+    const STATUS_IN_TRANSIT       = 2;
+    const STATUS_DELIVERY_PENDING = 3;
+    const STATUS_DELIVERED        = 4;
+    const STATUS_RETURNED         = 5;
+    const STATUS_UNKNOWN_OBJ      = 6;
+
+    private $baseUrl        = 'https://dpd.pt/track-and-trace';
+    private $possibleStatus = [
+        'Objeto aceite'                   => self::STATUS_PENDING,
+        'Objeto expedido'                 => self::STATUS_IN_TRANSIT,
+        'Objeto em distribuição'          => self::STATUS_IN_TRANSIT,
+        'Disponível para Levantamento'    => self::STATUS_DELIVERY_PENDING,
+        'Objeto com tentativa de entrega' => self::STATUS_DELIVERY_PENDING,
+        'Objeto entregue'                 => self::STATUS_DELIVERED,
+        'Objeto devolvido'                => self::STATUS_RETURNED,
+        'Objeto não encontrado'           => self::STATUS_UNKNOWN_OBJ,
+    ];
+
+    /**
+     * @param array $refs
+     * @return array
+     */
+    public function trackObjects(array $refs = [])
+    {
+        $returnArr   = [];
+        $requestRefs = '';
+
+        $j = 1;
+        foreach ($refs as $idx => $ref) {
+            $requestRefs .= ($j++ > 1 ? ',' : '') . $ref;
+
+            if ($j == $this->refsPerRequest) {
+                $returnArr += $this->makeRequest($requestRefs);
+                if ((int) $this->waitAfterRequest > 0) {
+                    sleep($this->waitAfterRequest);
+                }
+                $requestRefs = '';
+                $j           = 1;
+            }
+        }
+
+        if ($requestRefs != "") {
+            $returnArr += $this->makeRequest($requestRefs);
+        }
+
+        return $returnArr;
+    }
+
+    /**
+     * @param $statusCode
+     * @return string
+     */
+    public static function getStatusString($statusCode)
+    {
+        $class     = new \ReflectionClass(__CLASS__);
+        $constants = array_flip($class->getConstants());
+
+        return $constants[$statusCode];
+    }
+
+    /**
+     * @param $requestRefs
+     * @return array
+     */
+    private function makeRequest($requestRefs)
+    {
+        $returnArr = [];
+        $client    = new Client();
+        $res       = $client->request('POST', $this->baseUrl, [
+            'showResults' => true,
+            'reference'   => $requestRefs,
+        ]);
+
+        $res->filterXPath('//*[@class="table-responsive"]/table/tr')->each(function ($node, $i) use (&$returnArr) {
+            $resultText = trim($node->text());
+            if (strpos($resultText, '[+]Info') !== false) {
+
+                foreach ($this->possibleStatus as $statusText => $status) {
+                    if (strpos($resultText, $statusText) !== false) {
+                        $objRef                   = trim(substr($resultText, 0, strpos($resultText, ' ') - 1));
+                        $returnArr[trim($objRef)] = ['status' => $status, 'statusText' => $statusText];
+                    }
+                }
+            }
+        });
+
+        return $returnArr;
+    }
+
+}
